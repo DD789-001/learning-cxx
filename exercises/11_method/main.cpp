cb63b6ac00598299f5ae298d8d1d9fbddd25612f
xiaokeleeer <15840740+xiaokeleeer@user.noreply.gitee.com>
2026-08-19 10:10:25 +0800

    feat: finish learn 11 (improve)

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -6,6 +6,13 @@ struct Fibonacci {
 
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
+        if (i < 0) {
+        throw std::out_of_range("下标不能是负数");
+        }
+
+        if (i >= 128) {
+        throw std::out_of_range("下标超出 cache 范围");
+        }
 
         if (i < cached) {
             return cache[i];
@@ -13,7 +20,6 @@ struct Fibonacci {
             for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
             }
-            cached = i;
         }
 
         return cache[i];
